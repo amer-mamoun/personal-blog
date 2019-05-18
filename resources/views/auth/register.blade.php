ef687daf6d112507f31bc7b5c37dce1826d82c77
Amer Mamoun <amer.mn.96@gmail.com>
2019-05-18 20:22:42 +0200

    Admin panel author, users.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
+    <!--
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -74,4 +75,59 @@
         </div>
     </div>
 </div>
+     -->
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card p-4">
+                    <div class="card-header text-center text-uppercase h4 font-weight-light">
+                        Register
+                    </div>
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                    <div class="card-body py-5">
+                        <div class="form-group">
+                            <label class="form-control-label">Name</label>
+                            <input type="text" value="{{ old('name') }}" name="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}">
+                            @if($errors->has('name'))
+                                <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label class="form-control-label">Email</label>
+                            <input type="email" name="email" value="{{ old('email') }}" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}">
+                            @if($errors->has('email'))
+                                <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label class="form-control-label">Password</label>
+                            <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
+                            @if($errors->has('password'))
+                                <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label class="form-control-label">Confirm Password</label>
+                            <input type="password" name="password_confirmation" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-success btn-block">Create Account</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
